25c1a42eb9065ae6e6058785a78a095e019c13bc
miclan <miclan@163.com>
2014-09-05 14:30:04 +0800

    war

--- src/main/java/com/huawei/tjspider/PageController.java
+++ src/main/java/com/huawei/tjspider/PageController.java
@@ -45,42 +45,11 @@ public class PageController {
 
 		String srcHtml = jsoupResponse.body();
 		Document doc = Jsoup.parse(srcHtml);
-		String srcHtmlCharset = getHtmlCharset(doc);
 		String docHtml = doc.html();
 		
-//		Pattern pCharset = Pattern.compile("charset=([A-Za-z0-9\\-]+)");
-//		Element elementCharset = doc.getElementsByAttributeValueMatching("content", pCharset).get(0);
-//		logger.info("*****" + elementCharset.outerHtml());
-//		doc.getElementsByAttributeValueMatching("content", pCharset).get(0).remove();
-//		srcHtml = doc.html();
-//		logger.info("*****" + srcHtml.indexOf(elementCharset.outerHtml()));
-//		srcHtml.replaceAll(elementCharset.outerHtml(), "");
-		
-
-		String finalCharset = "UTF-8";
-		if (srcHtmlCharset != null || !"".equalsIgnoreCase(srcHtmlCharset)) {
-			finalCharset = srcHtmlCharset;
-		} else if (srcCharset != null || !"".equalsIgnoreCase(srcCharset)) {
-			finalCharset = srcCharset;
-		}
-		logger.info("finalCharset = " + finalCharset);
-
-//		String utf8Html = new String(srcHtml.getBytes(finalCharset), "UTF-8");
-//		String utf8Html = new String(srcHtml.getBytes("utf-8"), "gbk");
-//		String utf8Html = srcHtml;
-		
-		response.setContentType("text/html; charset=UTF-8");
-//		response.setContentType("text/html; charset=GBK");
+		response.setContentType("text/html; charset=utf-8");
 		response.getWriter().write(docHtml);
 		response.getWriter().close();
 		return null;
-	}
-
-	private String getHtmlCharset(Document doc) {
-		Pattern pCharset = Pattern.compile("charset=([A-Za-z0-9\\-]+)");
-		Element elementCharset = doc.getElementsByAttributeValueMatching("content", pCharset).get(0);
-		logger.info(elementCharset.outerHtml());
-		String charset = elementCharset.attr("content").split(";")[1].split("=")[1].trim().toUpperCase();
-		return charset;
 	}
 }
