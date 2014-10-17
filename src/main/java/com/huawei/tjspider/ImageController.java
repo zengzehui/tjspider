20f634191782e818af71c88793576226c96c4125
miclan <miclan@163.com>
2014-10-17 11:21:26 +0800

    优化日志打印，优化serving image file.

--- src/main/java/com/huawei/tjspider/ImageController.java
+++ src/main/java/com/huawei/tjspider/ImageController.java
@@ -35,7 +35,7 @@ public class ImageController {
 	@RequestMapping(value = "/0917", method = RequestMethod.GET)
 	public void getImg0917(@RequestParam("url") String url, @RequestParam("referer") String referer,
 			HttpServletResponse response) throws IOException {
-		logger.info(url + " getImg STARTS =====");
+		logger.info(url + " STARTS =====");
 		HttpGet httpget = new HttpGet(url);
 		httpget.setHeader("Referer", referer);
 		httpget.setHeader("User-Agent",
@@ -89,7 +89,7 @@ public class ImageController {
 				response.setStatus(404);
 			}
 		} catch (Exception e) {
-			logger.info(url + " getImg Exception *****");
+			logger.info(url + " Exception *****");
 			e.printStackTrace();
 			response.setStatus(404);
 		} finally {
@@ -99,13 +99,13 @@ public class ImageController {
 				bos.close();
 			httpclient.close();
 		}
-		logger.info(url + " getImg ENDS -----");
+		logger.info(url + " ENDS -----");
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public void getImg(@RequestParam("url") String url, @RequestParam("referer") String referer,
 			HttpServletResponse response) throws IOException {
-		logger.info(url + " getImg STARTS =====");
+		logger.info(url + " STARTS =====");
 		HttpGet httpget = new HttpGet(url);
 		httpget.setHeader("Referer", referer);
 		httpget.setHeader("User-Agent",
@@ -133,7 +133,7 @@ public class ImageController {
 				bosFile = new BufferedOutputStream(new FileOutputStream(imgFile));
 				srcResponse.getEntity().writeTo(bosFile);
 				bosFile.close();
-				logger.info(url + " getImg write to " + imgFile.getAbsolutePath());
+				logger.info(url + " write to " + imgFile.getAbsolutePath());
 				response.setStatus(200);
 				Header[] allHeaders = srcResponse.getAllHeaders();
 				for (int i = 0; i < allHeaders.length; i++) {
@@ -148,7 +148,7 @@ public class ImageController {
 				}
 				response.flushBuffer();
 			} else {
-				logger.info(url + " getImg Source Response is " + srcResponse.getStatusLine().toString());
+				logger.info(url + " Source Response is " + srcResponse.getStatusLine().toString());
 				response.sendError(404);
 				Header[] allHeaders = srcResponse.getAllHeaders();
 				for (int i = 0; i < allHeaders.length; i++) {
@@ -156,11 +156,11 @@ public class ImageController {
 				}
 			}
 		} catch (Exception e) {
-			logger.info(url + " getImg Exception *****");
+			logger.info(url + " Exception *****");
 			e.printStackTrace();
 			if (imgFile.exists()) {
 				imgFile.delete();
-				logger.info(url + " getImg Deleted *****");
+				logger.info(url + " Deleted *****");
 			}
 			response.sendError(404);
 		} finally {
@@ -176,7 +176,103 @@ public class ImageController {
 			httpclient.close();
 		}
 
-		logger.info(url + " getImg ENDS -----");
+		logger.info(url + " ENDS -----");
 	}
 
+	@RequestMapping(value = "/1017", method = RequestMethod.GET)
+	public void getImg1017(@RequestParam("url") String url, @RequestParam("referer") String referer,
+			HttpServletResponse response) throws IOException {
+		
+		String shortUrl = url.substring(url.lastIndexOf("/"));
+		logger.info(shortUrl + " STARTS ===== " + url);
+
+		DateTime now = new DateTime();
+		DateTimeFormatter dtDtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+
+		File imgFile = new File(Thread.currentThread().getContextClassLoader().getResource("").getPath() + "/image/"
+				+ dtDtf.print(now) + "/" + URLEncoder.encode(url, "UTF-8"));
+		File imgFolder = imgFile.getParentFile();
+		if (!imgFolder.exists()) {
+			imgFolder.mkdirs();
+		}
+		if (!imgFile.exists()) {
+			logger.info(shortUrl + " Start getting image.");
+			CloseableHttpResponse srcResponse = null;
+			BufferedOutputStream bosFile = null;
+			CloseableHttpClient httpclient = null;
+			try {
+				HttpGet httpget = new HttpGet(url);
+				httpget.setHeader("Referer", referer);
+				httpget.setHeader("User-Agent",
+						"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.103 Safari/537.36");
+
+				httpget.setConfig(RequestConfig.custom().setConnectTimeout(30 * 1000).setSocketTimeout(90 * 1000)
+						.setConnectionRequestTimeout(10 * 1000).build());
+
+				httpclient = HttpClients.createDefault();
+				srcResponse = httpclient.execute(httpget);
+				if (srcResponse.getStatusLine().getStatusCode() == 200) {
+					bosFile = new BufferedOutputStream(new FileOutputStream(imgFile));
+					srcResponse.getEntity().writeTo(bosFile);
+					bosFile.close();
+					srcResponse.close();
+					httpclient.close();
+					logger.info(shortUrl + " Writed to " + imgFile.getAbsolutePath() + " size=" + imgFile.length());
+				} else {
+					response.sendError(404, "Original response IS NOT 200.");
+					logger.info(shortUrl + " Response 404: Original response is not 200.");
+					return;
+				}
+				srcResponse.close();
+				httpclient.close();
+			} catch (Exception e) {
+				logger.info(shortUrl + " Exception: Failed to get image or write image to file.");
+				e.printStackTrace();
+				if (imgFile.exists()) {
+					imgFile.delete();
+				}
+			} finally {
+				if (bosFile != null) {
+					bosFile.close();
+				}
+				if (srcResponse != null) {
+					srcResponse.close();
+				}
+				if (httpclient != null) {
+					httpclient.close();
+				}
+				logger.info(shortUrl + " Finally: getting and writing image to file.");
+			}
+		} else {
+			logger.info(shortUrl + " Skip getting image.");
+		}
+		if (!imgFile.exists()) {
+			response.sendError(404, "Original response IS 200, but FAILED to write image to file.");
+			logger.info(shortUrl + " Response 404: Original response is not 200.");
+			return;
+		} else {
+			logger.info(shortUrl + " Start serving image.");
+			BufferedInputStream bisFile = null;
+			BufferedOutputStream bosResponse = null;
+			try {
+				bisFile = new BufferedInputStream(new FileInputStream(imgFile));
+				response.setContentLength((int) imgFile.length());
+				bosResponse = new BufferedOutputStream(response.getOutputStream());
+				byte[] buff = new byte[4096];
+				int bytesRead;
+				while (-1 != (bytesRead = bisFile.read(buff, 0, buff.length))) {
+					bosResponse.write(buff, 0, bytesRead);
+				}
+				bosResponse.close();
+				response.flushBuffer();
+				bisFile.close();
+			} catch (Exception e) {
+				logger.info(shortUrl + " Exception: FAILED to serve image to client.");
+				e.printStackTrace();
+			} finally {
+				logger.info(shortUrl + " Finally: serving image to client.");
+			}
+			logger.info(shortUrl + " ENDS =======");
+		}
+	}
 }
